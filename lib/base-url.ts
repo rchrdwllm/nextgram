0c79b8716bd63bbd2d86a303da5e17833dfe0be8
Richard William <rwilliam.flores@gmail.com>
2024-08-08 02:30:57 +0800

    chore: Refactor base-url to remove unnecessary protocol in return statement

--- lib/base-url.ts
+++ lib/base-url.ts
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (process.env.VERCEL_URL) return `https://${process.env.DOMAIN_URL}`;
+  if (process.env.VERCEL_URL) return `${process.env.DOMAIN_URL}`;
   return `http://localhost:3000`;
 }
