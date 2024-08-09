b6a5e1946f1633c695df59c87da9c59ac5a9e016
Richard William <rwilliam.flores@gmail.com>
2024-08-09 21:46:46 +0800

    Removed unused console statement

--- app/(user)/user/[id]/page.tsx
+++ app/(user)/user/[id]/page.tsx
@@ -6,8 +6,6 @@ const UserPage = async ({ params: { id } }: { params: { id: string } }) => {
   const { success: user, error } = await getUserById(id);
   const session = await auth();
 
-  console.log(session);
-
   if (error) {
     return <div>Error: {error}</div>;
   }
